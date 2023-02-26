64e36159c0603a0d48ac706c0b218b39a06ed205
ayedavanita <126408327+ayedavanita@users.noreply.github.com>
2023-02-26 13:57:07 -0300

    Add files via upload

---
+++ funciones_para_el_cv.js
@@ -0,0 +1,35 @@
+document.getElementById('boton_exactas').addEventListener('click', function () {
+    location.href="https://www.mdp.edu.ar/exactas/"
+}) ;
+
+document.getElementById('boton_ing').addEventListener('click', function () {
+    location.href="https://www.fi.mdp.edu.ar/"
+}) ;
+
+document.getElementById('boton_ipr').addEventListener('click', function () {
+    location.href="https://www.iprmardelplata.edu.ar/"
+}) ;
+
+document.getElementById('boton_minerva').addEventListener('click', function () {
+    location.href="https://www.institutominerva.edu.ar/"
+}) ;
+
+document.getElementById('boton_ees72').addEventListener('click', function () {
+    location.href="https://www.facebook.com/people/EES-72/100054274730281/"
+}) ;
+
+document.getElementById('boton_ees1').addEventListener('click', function () {
+    location.href="https://www.facebook.com/EscuelaPilotoMarDelPlata/?locale=es_LA"
+}) ;
+
+document.getElementById('boton_iae').addEventListener('click', function () {
+    location.href="https://iae.com.ar/"
+}) ;
+
+document.getElementById('boton_iam').addEventListener('click', function () {
+    location.href="https://www.argentinomodelo.edu.ar/"
+}) ;
+
+document.getElementById('boton_idra').addEventListener('click', function () {
+    location.href="https://idravirtual.edu.ar/"
+}) ;
